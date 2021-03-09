84adc9ad25a4cec4413d108df202689cf48a3176
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-03-09 22:05:46 +0200

    initial3

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 ///
 const gameConteiner = document.querySelector(".game-container");
 const eggBoxEl = document.getElementById("eggBox");
+const boxContainer = document.querySelector(".box-wrapper");
 ///
 
 //angle of box
 let rotateX = 0;
 let rotateY = 0;
 const acceptanceAngle = 70;
+//
+let gameContainerPos = gameConteiner.getBoundingClientRect();
+window.onresize = () =>
+  (gameContainerPos = gameConteiner.getBoundingClientRect());
+
+let boxX, boxY;
+
+let isTimerOn = false;
 
 const angleChecker = angle => {
   if (Math.abs(angle) > acceptanceAngle)
     angle = angle > 0 ? acceptanceAngle : -acceptanceAngle;
   return angle;
 };
-let inc = 0;
-gameConteiner.addEventListener("mousemove", e => {
-  //console.log(eggBoxEl.getBoundingClientRect().left, e.pageX);
+
+const posChecker = (x, y) =>
+  x > gameContainerPos.left &&
+  x < gameContainerPos.right - 70 &&
+  y > gameContainerPos.top &&
+  y < gameContainerPos.bottom - 70;
+function moveBoxTo(x, y) {
+  if (!isTimerOn && posChecker(x, y)) {
+    isTimerOn = true;
+    boxContainer.style.top = `${y}px`;
+    boxContainer.style.left = `${x}px`;
+    setTimeout(() => {
+      isTimerOn = false;
+    }, 100);
+  }
+}
+
+document.addEventListener("mousemove", e => {
+  boxX = e.pageX;
+  boxY = e.pageY;
+  moveBoxTo(e.pageX, e.pageY);
 
   rotateX = angleChecker(e.pageX - eggBoxEl.getBoundingClientRect().left);
-
   rotateY = angleChecker(-e.pageY + eggBoxEl.getBoundingClientRect().top) + 20;
-  console.log(rotateY);
   if (rotateY > 0) rotateY = rotateY / 4;
   eggBoxEl.style.transform = `rotateX(${rotateY}deg) rotateZ(${rotateX}deg)`;
-
-  
-  eggBoxEl.style.top = `${e.pageY}px`;
-  eggBoxEl.style.left = `${e.pageX}px`;
 });
