58d404780f09467b4b122e34e7d0f0e4f9e90e04
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-03-09 18:05:46 +0200

    initial1

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var cube = document.getElementById("cube");
-document.addEventListener("mousemove", e => {
-  var transform = `rotateX(${e.pageY}deg) rotateY(${e.pageX}deg)`;
-  cube.style.transform = transform;
+///
+const gameConteiner = document.querySelector(".game-container");
+const eggBoxEl = document.getElementById("eggBox");
+
+///
+
+gameConteiner.addEventListener("mousemove", e => {
+  eggBoxEl.style.transform = `rotateX(${e.pageY}deg) rotateY(${e.pageX}deg)`;
+  console.log(eggBoxEl.getBoundingClientRect().left, e.pageX);
+//   eggBoxEl.style.top = `${e.pageY}px`;
+//   eggBoxEl.style.left = `${e.pageX}px`;
 });
