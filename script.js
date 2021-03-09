e9484e21b36b7bfc0a69b024b785d68ace175670
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-03-09 19:54:55 +0200

    initial2

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 ///
 const gameConteiner = document.querySelector(".game-container");
 const eggBoxEl = document.getElementById("eggBox");
-
 ///
 
+//angle of box
+let rotateX = 0;
+let rotateY = 0;
+const acceptanceAngle = 70;
+
+const angleChecker = angle => {
+  if (Math.abs(angle) > acceptanceAngle)
+    angle = angle > 0 ? acceptanceAngle : -acceptanceAngle;
+  return angle;
+};
+let inc = 0;
 gameConteiner.addEventListener("mousemove", e => {
-  eggBoxEl.style.transform = `rotateX(${e.pageY}deg) rotateY(${e.pageX}deg)`;
-  console.log(eggBoxEl.getBoundingClientRect().left, e.pageX);
-//   eggBoxEl.style.top = `${e.pageY}px`;
-//   eggBoxEl.style.left = `${e.pageX}px`;
+  //console.log(eggBoxEl.getBoundingClientRect().left, e.pageX);
+
+  rotateX = angleChecker(e.pageX - eggBoxEl.getBoundingClientRect().left);
+
+  rotateY = angleChecker(-e.pageY + eggBoxEl.getBoundingClientRect().top) + 20;
+  console.log(rotateY);
+  if (rotateY > 0) rotateY = rotateY / 4;
+  eggBoxEl.style.transform = `rotateX(${rotateY}deg) rotateZ(${rotateX}deg)`;
+
+  
+  eggBoxEl.style.top = `${e.pageY}px`;
+  eggBoxEl.style.left = `${e.pageX}px`;
 });
