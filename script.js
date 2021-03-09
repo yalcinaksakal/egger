43e29827beff2e33e50769ae796f2cb9420d13f8
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-03-09 23:23:37 +0200

    eggs1

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const gameConteiner = document.querySelector(".game-container");
 const eggBoxEl = document.getElementById("eggBox");
 const boxContainer = document.querySelector(".box-wrapper");
 ///
-
+//speed
+const speedX = 5;
+const speedY = 7;
 //angle of box
 let rotateX = 0;
 let rotateY = 0;
@@ -13,9 +15,8 @@ let gameContainerPos = gameConteiner.getBoundingClientRect();
 window.onresize = () =>
   (gameContainerPos = gameConteiner.getBoundingClientRect());
 
-let boxX, boxY;
-
-let isTimerOn = false;
+let boxNextX = boxContainer.getBoundingClientRect().left,
+  boxNextY = boxContainer.getBoundingClientRect().top;
 
 const angleChecker = angle => {
   if (Math.abs(angle) > acceptanceAngle)
@@ -23,29 +24,50 @@ const angleChecker = angle => {
   return angle;
 };
 
-const posChecker = (x, y) =>
-  x > gameContainerPos.left &&
-  x < gameContainerPos.right - 70 &&
-  y > gameContainerPos.top &&
-  y < gameContainerPos.bottom - 70;
-function moveBoxTo(x, y) {
-  if (!isTimerOn && posChecker(x, y)) {
-    isTimerOn = true;
-    boxContainer.style.top = `${y}px`;
-    boxContainer.style.left = `${x}px`;
-    setTimeout(() => {
-      isTimerOn = false;
-    }, 100);
-  }
+let directionX, directionY, boxX, boxY;
+
+function moveBoxTo() {
+  //   if (!isTimerOn && posChecker(x, y)) {
+  //     isTimerOn = true;
+
+  //     setTimeout(() => {
+  //       isTimerOn = false;
+  //     }, 100);
+  //
+  boxX = boxContainer.getBoundingClientRect().left;
+  boxY = boxContainer.getBoundingClientRect().top;
+  directionX = boxNextX - boxX;
+  directionY = boxNextY - boxY;
+  directionX = Math.abs(directionX) > speedX * 10 ? directionX : 0;
+  directionY = Math.abs(directionY) > speedY * 10 ? directionY : 0;
+
+  if (directionX) directionX = directionX > 0 ? speedX : -speedX;
+  if (directionY) directionY = boxNextY - boxY > 0 ? speedY : -speedY;
+
+  directionX += boxX;
+  directionY += boxY;
+  if (
+    directionX > gameContainerPos.left &&
+    directionX < gameContainerPos.right - 70
+  )
+    boxContainer.style.left = `${directionX}px`;
+  if (
+    directionY > gameContainerPos.top &&
+    directionY < gameContainerPos.bottom - 100
+  )
+    boxContainer.style.top = `${directionY}px`;
+
+  window.requestAnimationFrame(moveBoxTo);
 }
 
 document.addEventListener("mousemove", e => {
-  boxX = e.pageX;
-  boxY = e.pageY;
-  moveBoxTo(e.pageX, e.pageY);
+  boxNextX = e.pageX;
+  boxNextY = e.pageY;
 
+  //rotate box towards mouse
   rotateX = angleChecker(e.pageX - eggBoxEl.getBoundingClientRect().left);
   rotateY = angleChecker(-e.pageY + eggBoxEl.getBoundingClientRect().top) + 20;
   if (rotateY > 0) rotateY = rotateY / 4;
   eggBoxEl.style.transform = `rotateX(${rotateY}deg) rotateZ(${rotateX}deg)`;
 });
+moveBoxTo();
