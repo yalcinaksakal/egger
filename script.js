863327191da2a5ad43f2cf21d008151b7860d629
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-03-10 02:15:11 +0200

    eggs3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ///
-const gameConteiner = document.querySelector(".game-container");
+const gameContainer = document.querySelector(".game-container");
 const eggBoxEl = document.getElementById("eggBox");
 const boxContainer = document.querySelector(".box-wrapper");
 ///
@@ -11,23 +11,38 @@ let rotateX = 0;
 let rotateZ = 0;
 const acceptanceAngle = 50;
 //
-let gameContainerPos = gameConteiner.getBoundingClientRect();
+const eggArray = [];
+const eggColors = [
+  "whitesmoke",
+  "rgb(205, 208, 159)",
+  "rgb(129, 223, 224)",
+  "rgba(81, 81, 81, 0.666)",
+  "rgb(96, 49, 41)",
+];
+let score = 0;
+//
+let gameContainerPos = gameContainer.getBoundingClientRect();
 window.onresize = () =>
-  (gameContainerPos = gameConteiner.getBoundingClientRect());
+  (gameContainerPos = gameContainer.getBoundingClientRect());
 
 let boxNextX = boxContainer.getBoundingClientRect().left,
   boxNextY = boxContainer.getBoundingClientRect().top;
 
 let directionX, directionY, boxX, boxY;
 
-function moveBoxTo() {
-  //   if (!isTimerOn && posChecker(x, y)) {
-  //     isTimerOn = true;
+function moveEggs() {
+  eggArray.forEach(egg => {
+    if (!egg.broken) {
+      egg.left += egg.speedX;
+      egg.top += egg.speedY;
+      const eggEl = document.getElementById(egg.el);
+      eggEl.style.top = `${egg.top}px`;
+      eggEl.style.left = `${egg.left}px`;
+    }
+  });
+}
 
-  //     setTimeout(() => {
-  //       isTimerOn = false;
-  //     }, 100);
-  //
+function moveBoxTo() {
   boxX = boxContainer.getBoundingClientRect().left;
   boxY = boxContainer.getBoundingClientRect().top;
   directionX = boxNextX - boxX;
@@ -50,7 +65,7 @@ function moveBoxTo() {
     directionY < gameContainerPos.bottom - 100
   )
     boxContainer.style.top = `${directionY}px`;
-
+  moveEggs();
   window.requestAnimationFrame(moveBoxTo);
 }
 
@@ -70,4 +85,33 @@ document.addEventListener("mousemove", e => {
 
   eggBoxEl.style.transform = `rotateX(${rotateX}deg) rotateZ(${rotateZ}deg)`;
 });
+
+function egger() {
+  let rndm = Math.floor(Math.random() * 5);
+  const eggProperties = {
+    color: eggColors[rndm],
+    speedX: rndm * -2,
+    speedY: rndm > 2 ? -rndm : rndm,
+  };
+
+  const newEgg = document.createElement("i");
+  newEgg.classList.add("fas", "fa-egg");
+  newEgg.style.position = "absolute";
+
+  newEgg.style.color = eggProperties.color;
+
+  eggProperties.left = gameContainerPos.right - 30;
+  newEgg.style.left = `${eggProperties.left}px`;
+
+  eggProperties.top = gameContainerPos.top + 200;
+  newEgg.style.top = `${eggProperties.top}px`;
+
+  gameContainer.appendChild(newEgg);
+  eggProperties.broken = 0;
+  eggArray.push(eggProperties);
+  newEgg.id = eggArray.length;
+  eggProperties.el = newEgg.id;
+}
+
+egger();
 moveBoxTo();
